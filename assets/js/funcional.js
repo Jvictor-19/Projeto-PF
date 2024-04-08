dafa32f94979cd352598220ef784fa4bc508fa35
eulercruz458 <164105083+eulercruz458@users.noreply.github.com>
2024-04-08 16:41:03 -0300

    funcional.js
    SIIIIIU Receba, Obrigado meu Deus

--- assets/js/funcional.js
+++ assets/js/funcional.js
@@ -16,3 +16,26 @@ const pegarCSV = () => {
       throw new Error(`Erro ao buscar dados do CSV: ${error.message}`);
     });
 };
+
+
+function pesquisar1(){
+   // Cidade e Pais escolhidos pelo usuário.
+  var cidade = document.getElementById("searchInput1.1").value.trim().toLowerCase()
+  var pais = document.getElementById("searchInput1.2").value.trim().toLowerCase()
+
+  pegarCSV()
+    .then(resultados => {
+      const filtrado4 = resultados.filter((x) => x.Team.toLowerCase() === pais)
+      .filter((x) => x.City.toLowerCase() === cidade)
+      .map((x) => (x.Medal != "NA" ? 1 : 0))
+      .reduce((acc, x) => acc + x, 0)
+
+      if (filtrado4 > 0) { 
+        document.getElementById("resultadoPesquisa1").innerHTML = `O País ${pais} obteve ${filtrado4} medalhas nas Olimpiadas de ${cidade}`
+      } else {
+        document.getElementById("resultadoPesquisa1").innerHTML = `O País ${pais} não obteve medalhas nas Olimpiadas de ${cidade}.`;
+      }
+  })
+    .catch(error => console.error('Erro:', error));
+}
+
